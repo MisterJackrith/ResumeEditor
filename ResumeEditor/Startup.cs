2969f5bee81ddb0751504874cfb8928a6b085f73
MisterJackrith <mr.jackrith@gmail.com>
2017-02-22 14:53:58 +0700

    Updae nuget!

---
+++ ResumeEditor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ResumeEditor.Startup))]
+namespace ResumeEditor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
