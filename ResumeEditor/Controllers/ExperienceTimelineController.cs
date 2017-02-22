d5e47ab57f86aab0aef1631ce77cdea2566985a1
MisterJackrith <mr.jackrith@gmail.com>
2017-02-22 14:56:45 +0700

    New Controller

---
+++ ResumeEditor/Controllers/ExperienceTimelineController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ResumeEditor.Controllers
+{
+    public class ExperienceTimelineController : Controller
+    {
+        // GET: ExperienceTimeline
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
